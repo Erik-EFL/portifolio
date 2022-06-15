15e18057354f3e922d7e990f0df86f0623f5108b
Erik-EFL <erik.lima.f@outlook.com>
2022-06-15 16:05:11 -0300

    Intalando e configurando Styled-components

---
+++ src/public/StyledComponents/Links.jsx
@@ -0,0 +1,9 @@
+import Styled from 'styled-components';
+
+export const LinksH = Styled.a`
+
+`;
+
+export const Logo = Styled.a`
+  
+`;
---
+++ src/public/StyledComponents/Palets.jsx
@@ -0,0 +1,21 @@
+const palets = {
+  /* font-family: 'Gruppo' */
+  /* paleta 1 */
+  background: '#393E46',
+  titles: '#00adb5',
+  text: '#aad8d3',
+  /* paleta 2 */
+  background2: '#0d0d0d',
+  titles2: '#d95032',
+  text2: '#f2f2f2',
+  /* paleta 3 */
+  background3: '#010101',
+  titles3: '#a60311',
+  text3: '#abc5cf',
+  /* paleta 4 */
+  background4: '#2180a6',
+  titles4: '#f2f2f2',
+  text4: '#96e0f0',
+};
+
+module.exports = palets;
